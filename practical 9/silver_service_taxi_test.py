537a6942cae2e5d3836b301b66c2be9be720eb49
tamil290 <tamilselvan.anbalagan@my.jcu.edu.au>
2023-05-20 13:41:42 +0800

    prac 09 feedback

--- practical 9/silver_service_taxi_test.py
+++ practical 9/silver_service_taxi_test.py
@@ -1,7 +1,7 @@
 from silver_service_taxi import SilverServiceTaxi
 
 # create a new SilverServiceTaxi called "Fancy Taxi", with 100 units of fuel, and fanciness of 2
-fancy_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2)
+fancy_taxi = SilverServiceTaxi("Fancy Taxi", 200, 2)
 
 # drive the taxi 18km
 fancy_taxi.drive(18)
--- practical 9/unreliable_car_test.py
+++ practical 9/unreliable_car_test.py
@@ -5,3 +5,6 @@ my_car = UnreliableCar("My Car", 100, 50)
 
 # Print the car's details
 print(my_car)  # Output: Car: My Car, Fuel: 100, Reliability: 50
+
+distance_driven = my_car.drive(50)
+print("Distance driven:", distance_driven)
